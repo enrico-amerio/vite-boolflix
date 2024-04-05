02f30f7d2eb63951e25a34b62b18f89427aa1e45
enrico-amerio <amerio.enrico95@gmail.com>
2024-04-05 10:17:13 +0200

    [add] Card and header styling

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,9 +7,9 @@ import {store} from '../data/store'
       }
     },
     methods:{
-      search(){
+      search(nameToSearch){
         this.store.searchParams.totPages = 0
-        this.store.searchParams.query = this.nameToSearch
+        this.store.searchParams.query = nameToSearch
         this.$emit('search');
       }
     }
@@ -19,15 +19,31 @@ import {store} from '../data/store'
 
 
 <template>
-<nav>
-  <a class="navbar-brand" href="#">BOOLFLIX</a>
-    <input class="form-control me-2" type="search" placeholder="Search" v-model="nameToSearch">
-    <button class="btn btn-outline-success" @click="search(nameToSearch)">Search</button>
+<nav class="d-flex flex-column justify-content-center ">
+  <div class="row">
+    <div class="col-9"> 
+      <a class="navbar-brand text-danger ms-2 h1 logo" href="#">BOOLFLIX</a>
+    </div>
+    <div class="col d-flex">
+      <input class="form-control me-2" type="search" placeholder="Search" v-model="nameToSearch">
+      <button class="btn btn-danger" @click="search(nameToSearch)">Search</button>
+    </div>
+  </div>
 </nav>
 </template>
 
 
 
 <style lang="scss" scoped>
-
+nav{
+  background-color: black;
+  position:fixed;
+  z-index: 999;
+  width: 100%;
+  height: 50px;
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
+    .logo{
+    font-size: 1.8rem;
+    }
+}
 </style>
--- src/components/partials/Card.vue
+++ src/components/partials/Card.vue
@@ -28,21 +28,77 @@ import {store} from '../../data/store';
   }
 </script>
 <template>
-  <div class="card" style="width: 18rem;">
-  <ul class="list-group list-group-flush">
-    <img v-if="img" class="card-img-top" :src="'https://image.tmdb.org/t/p/original/'+ img" :alt="img">
-    <li class="list-group-item">{{ title }}</li>
-    <li class="list-group-item">{{ originalTitle }}</li>
-    <li class="list-group-item" v-if="this.store.availableFlags.includes(language)"> <img class="flag" :src="'img/' + language + '.png'" alt=""> </li>
-    <li class="list-group-item" v-else> {{ language }}</li>
-    <li class="list-group-item" v-html="getVote(vote)"></li>
-  </ul>
+  
+  <div class="card m-3" style="width: 18rem;">
+    <img v-if="img" class="poster" :src="'https://image.tmdb.org/t/p/original/'+ img" :alt="img">
+    <h2 class="title">{{ title }}</h2>
+    <div class="movie-info">
+      <h5>Titolo originale: {{ originalTitle }}</h5>
+      <div v-if="this.store.availableFlags.includes(language)"> <img class="flag" :src="'img/' + language + '.png'" alt=""> </div>
+      <div v-else> {{ language }}</div>
+      <div v-html="getVote(vote)"></div>
+    </div>
 </div>
 </template>
 
 
 <style lang="scss" scoped>
-.flag{
-width: 20px;
+.card{
+  height: 500px;
+  border-radius: 10px;
+  overflow: hidden;
+  cursor: pointer;
+  position: relative;
+  color: white;
+  box-shadow: 0 10px 30px 5px rgba(0, 0, 0, 0.2);
+  background: grey;
+
+  .poster {
+    position: absolute;
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    opacity: 0.8;
+    transition: opacity .2s ease-out;
+  }
+  h2 {
+    position: absolute;
+    inset: auto auto 30px 30px;
+    margin: 0;
+    transition: inset .3s .3s ease-out;
+    font-weight: normal;
+    text-transform: uppercase;
+  }
+  .movie-info {
+    position: absolute;
+    opacity: 0;
+    max-width: 80%;
+    transition: opacity .3s ease-out;
+    inset: auto auto 80px 30px;
+    padding: 5px;
+    border-radius: 5px;
+  }
+  &:hover h2 {
+    inset: auto auto 220px 30px;
+    transition: inset .3s ease-out;
+    
+  }
+  &:hover .movie-info{
+    opacity: 1;
+    transition: opacity .5s .1s ease-in;
+  }
+  &:hover .poster {
+    transition: opacity .3s ease-in;
+    opacity: 1;
+    filter: brightness(50%);
+  }
+
+  .flag{
+  width: 20px;
+  }
+  
 }
+
 </style>
